e7006db78a7b5fc7b2d3f023442853a0580ba47f
UNCROOS <any492266@gmail.com>
2024-05-21 00:00:21 +0900

    init

--- myenv/speedat/users/urls.py
+++ myenv/speedat/users/urls.py
@@ -1,17 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 from .views import RegisterView, LoginView, UserView, LogoutView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('register', RegisterView.as_view()),   # 회원가입 엔드포인트
-    path('login', LoginView.as_view()),         # 로그인 엔드포인트
-    path('user', UserView.as_view()),           # 사용자 정보 및 조회 엔드포인트
-    path('logout', LogoutView.as_view()),       # 로그아웃 엔드포인트
-
-    # OAuth 및 JWT 관련 엔드포인트 추가
-    path('accounts/', include('allauth.urls')),  # allauth 경로 추가
-    path('auth/', include('dj_rest_auth.urls')),  # dj-rest-auth 경로 추가
-    path('auth/registration/', include('dj_rest_auth.registration.urls')),  # 회원가입 경로 추가
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # JWT 토큰 발급
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # JWT 토큰 갱신
+    path('register', RegisterView.as_view()),   #회원가입 엔드포인트
+    path('login', LoginView.as_view()),         #로그인 엔드포인트
+    path('user', UserView.as_view()),           #사용자 정보 및 조회 엔드포인트
+    path('logout', LogoutView.as_view()),       #로그아웃 엔드포인트
 ]
