76c6a871af970519937ed469aa67408354c7e7c5
UNCROOS <any492266@gmail.com>
2024-05-20 16:57:44 +0900

    init

--- myenv/speedat/users/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-# Create your models here.
-class User(AbstractUser):
-    name = models.CharField(max_length=255)         #사용자 이름 최대 길이 255
-    email = models.CharField(max_length=255, unique= True)      #사용자 이메일 최대 길이 255, 고유 값
-    password = models.CharField(max_length=255)     #비밀번호 최대 길이 255
-    USERNAME_FIELD = 'email'        #사용자 이름 필드를 이메일로 지정
-    REQUIRED_FIELDS = []        #필수 필드 설정
